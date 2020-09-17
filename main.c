2e162128ff95a1520f57838da7ca9bc6f3ed4023
AugustusP <agp5191@psu.edu>
2020-09-17 13:59:57 +0000

    input and outputs proper values

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
-// Author: Yanling Wang yuw17@psu.edu
+// Author: Augustus Perseghin agp5191
 // Collaborator:
 // Collaborator:
 // Collaborator:
-// Section: 1
-// Breakout: 12
+// Section: 4
+// Breakout: 15
 
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
 
 int sum_n(int);
+void print_n(char*,int);
 
 int main(void) {
-  int n = 0;
-  n = atof(readline("Enter an int: "));
-  printf("sum is %i.",sum_n(n));
+  int n_in = 0;
+  char* s_in = 0;
+  n_in = atof(readline("Enter an int: "));
+  printf("sum is %i.\n",sum_n(n_in));
+  s_in = readline("Enter a string: ");
+  print_n(s_in, n_in);
   return 0;
 }
 
@@ -30,7 +34,7 @@ void print_n(char* s, int n)
 {
   if(n > 0)
   {
-    printf("%s", s);
+    printf("%s\n", s);
     print_n(s,n-1);
   }
   return;
